68b43cf32261ec16f8168e2457096f1cf5fe2606
Mubashir <zamir.mubashir@gmail.com>
2024-10-24 10:53:18 +0100

    #individual-assessment:
    1. Eureka.
    2. Electricity provider service.

---
+++ individual-assessment/electricity-provider/src/test/java/com/smart_cities/electricity_provider/ElectricityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_cities.electricity_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
