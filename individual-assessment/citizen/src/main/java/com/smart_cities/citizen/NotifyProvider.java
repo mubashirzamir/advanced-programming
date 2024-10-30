0eadaf82f6d8cafe6962bf9add5f19d8669bd9e2
Mubashir <zamir.mubashir@gmail.com>
2024-10-30 19:44:53 +0000

    #individual-assessment: Improvements.

--- individual-assessment/citizen/src/main/java/com/smart_cities/citizen/NotifyProvider.java
+++ individual-assessment/citizen/src/main/java/com/smart_cities/citizen/NotifyProvider.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -38,7 +39,7 @@ public class NotifyProvider {
     public Long getMeterId() {
         return meterId;
     }
-
+    
     public void notify(int consumption) {
         ConsumptionPayload consumptionPayload = new ConsumptionPayload(
                 this.getCitizenId(),
